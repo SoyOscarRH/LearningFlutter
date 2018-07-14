33212961a8f545a420ab09ef89e93a17a6cf9b4c
Oscar Andrés Rosas <soyoscarrh@gmail.com>
2018-07-14 12:49:26 -0500

    Continue tutorial

--- Little Examples/startup_namer/lib/main.dart
+++ Little Examples/startup_namer/lib/main.dart
@@ -1,28 +1,84 @@
+// Step 4: Create an infinite scrolling lazily loaded list
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-
-
-    final someWordPair = WordPair.random();
-
-    return MaterialApp(
-      title: 'StartUpNamer',
+    return new MaterialApp(
+      theme: ThemeData.light(),
+      title: 'Startup Name Generator',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("StartUp Namer"),
-        ),
-        body: Center(
-          child: Text(someWordPair.asPascalCase),
-        )
-      )
+      home: new RandomWords(),
     );
   }
 }
 
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = List<WordPair>();
+  final _saved = Set<WordPair>();
+
+  final TextStyle _biggerFont = new TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w300,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(20.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+
+        final index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+
+    final alreadySaved = _saved.contains(pair);
+
+    return new ListTile(
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red.shade300 : null,
+      ),
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
